a52785d895363f7c88aa1e45132a87cbf3548366
Michael Smith <silentcoder99@gmail.com>
2021-08-13 17:44:06 +1000

    Add HoloArray package

---
+++ HoloArray.lua
@@ -0,0 +1,110 @@
+local Vec3 = require("Vec3")
+local component = require("component")
+local holo = component.hologram
+ 
+local holoResolution = Vec3:new(48, 32, 48)
+ 
+HoloArray = {}
+function HoloArray:new(size)
+    local projectors = {}
+    
+    for x = 1, size.x do
+        projectors[x] = {}
+        
+        for y = 1, size.y do
+            projectors[x][y] = {}
+        end
+    end
+    
+    local obj = {
+        size = size,
+        projectors = projectors
+    }
+    setmetatable(obj, self)
+    self.__index = self
+    return obj
+end
+ 
+function HoloArray:getResolution()
+    return self.size * holoResolution
+end
+ 
+function HoloArray:setProjectors(addresses)
+    local i = 1
+    for y = 1, self.size.y do
+        for z = 1, self.size.z do
+            for x = 1, self.size.x do
+                local address = component.get(addresses[i])
+                local proxy = component.proxy(address)
+                
+                self.projectors[x][y][z] = proxy
+                i = i + 1
+            end
+        end
+    end
+end
+ 
+function HoloArray:toProjectorSpace(pos)
+    return Vec3:new(
+        math.floor((pos.x - 1) / holoResolution.x) + 1,
+        math.floor((pos.y - 1) / holoResolution.y) + 1,
+        math.floor((pos.z - 1) / holoResolution.z) + 1
+    )
+end
+ 
+function HoloArray:toWorldSpace(pos)
+    return Vec3:new(
+        (pos.x - 1) * holoResolution.x + 1,
+        (pos.y - 1) * holoResolution.y + 1,
+        (pos.z - 1) * holoResolution.z + 1
+    )
+end
+ 
+-- Get the location of the point within it's respective projector
+function HoloArray:toLocalSpace(pos)
+    local projectorPos = self:toProjectorSpace(pos)
+    return pos - self:toWorldSpace(projectorPos) + Vec3:new(1, 1, 1)
+end
+ 
+function HoloArray:set(x, y, z, value)
+    local resolution = self:getResolution()
+    if x < 1 or x > resolution.x or
+            y < 1 or y > resolution.y or
+            z < 1 or z > resolution.z then
+        return nil
+    end
+    
+    local pos = Vec3:new(x, y, z)
+    local projectorPos = self:toProjectorSpace(pos)
+    local localPos = self:toLocalSpace(pos)
+    
+    local projector = self.projectors[projectorPos.x][projectorPos.y][projectorPos.z]
+    
+    projector.set(localPos.x, localPos.y, localPos.z, value)
+end
+ 
+-- Invoke a function on all projectors
+function HoloArray:invoke(funcName, ...)
+    
+    print("First arg", ...)
+    
+    for x = 1, self.size.x do
+        for y = 1, self.size.y do
+            for z = 1, self.size.z do
+                print(funcName, ...)
+                self.projectors[x][y][z][funcName](...)
+            end
+        end
+    end
+end
+ 
+function HoloArray:setPaletteColor(number, value)
+    print("Setting palette", number, value)
+    self:invoke("setPaletteColor", number, value)
+end
+ 
+function HoloArray:clear()
+    self:invoke("clear")
+end
+ 
+return HoloArray
